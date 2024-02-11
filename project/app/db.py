214ba1a2dffd4e1f45b2f3c24bc9233cc0e704eb
Sergei Svechnikov <srgcandle@mail.ru>
2024-02-11 23:21:28 +0400

    feat(product): Перенести инициализацию бд в отдельную функцию модуля db.py.

--- project/app/db.py
+++ project/app/db.py
@@ -1,5 +1,7 @@
 import os
 
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 TORTOISE_ORM = {
     'connections': {'default': os.environ.get('DATABASE_URL')},
@@ -10,3 +12,13 @@ TORTOISE_ORM = {
         },
     },
 }
+
+
+def initialize_database(app: FastAPI) -> None:
+    register_tortoise(
+        app=app,
+        db_url=os.environ.get('DATABASE_URL'),
+        modules={'models': ['app.models.tortoise']},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )

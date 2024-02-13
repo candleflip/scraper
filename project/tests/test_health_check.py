ad60ac5f741f4cc0a57816ab0ed9bf726eaa6394
Sergei Svechnikov <srgcandle@mail.ru>
2024-02-13 14:23:46 +0400

    fix(product): Поправить url ручки health_check.

--- project/tests/test_health_check.py
+++ project/tests/test_health_check.py
@@ -2,7 +2,7 @@ from app import main
 
 
 def test_health_check(test_app):
-    response = test_app.get('/ping')
+    response = test_app.get('/health_check')
     assert response.status_code == 200
     assert response.json() == {
         "ping": "pong",

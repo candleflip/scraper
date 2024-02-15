d51c93f29bd993b4d34ebc542c3a60bd13bed1a1
Sergei Svechnikov <srgcandle@mail.ru>
2024-02-15 15:44:20 +0400

    feat(product): Добавить тесты для ручки удаления summary.

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -23,8 +23,8 @@ def test_create_summary_invalid_json(test_app_with_db):
 
 def test_read_summary(test_app_with_db):
     response = test_app_with_db.post('/summaries/', data=json.dumps({'url': 'https://foo.bar'}))
-    summary_id = response.json()['id']
     assert response.status_code == 201
+    summary_id = response.json()['id']
 
     response = test_app_with_db.get(f'/summaries/{summary_id}')
     assert response.status_code == 200
@@ -56,3 +56,18 @@ def test_read_all_summaries(test_app_with_db):
 
     response_list = response.json()
     assert len(list(filter(lambda x: x['id'] in added_summaries, response_list))) == 2
+
+
+def test_remove_summary(test_app_with_db):
+    response = test_app_with_db.post('/summaries/', data=json.dumps({'url': 'https://foo.bar'}))
+    assert response.status_code == 201
+    summary_id = response.json()['id']
+
+    response = test_app_with_db.delete(f'/summaries/{summary_id}')
+    assert response.status_code == 200
+    assert response.json() == {'id': summary_id, 'url': 'https://foo.bar'}
+
+def test_remove_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.delete('/summaries/999')
+    assert response.status_code == 404
+    assert response.json()['detail'] == 'Summary not found'

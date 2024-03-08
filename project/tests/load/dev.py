a9f9f13694c1d0d12c2bb0ee55f54f9bd44011a3
Sergei Svechnikov <srgcandle@mail.ru>
2024-03-08 17:09:49 +0400

    style(tests): Убрать мертвый код в locust файле.

--- project/tests/load/dev.py
+++ project/tests/load/dev.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
-from typing import Tuple, List
-
-from locust import HttpUser, between, task, events, LoadTestShape, User
+from locust import HttpUser, between, task, events
 
 
 class WebUser(HttpUser):
@@ -13,25 +11,6 @@ class WebUser(HttpUser):
     @task
     def health_check(self):
         self.client.get('/health_check/')
-
-
-# class StageShape(LoadTestShape):
-#     stages = [
-#         {'duration': 20, 'users': 2, 'spawn_rate': 1},
-#         {'duration': 40, 'users': 4, 'spawn_rate': 1},
-#         {'duration': 60, 'users': 8, 'spawn_rate': 1},
-#         {'duration': 80, 'users': 16, 'spawn_rate': 1},
-#         {'duration': 100, 'users': 20, 'spawn_rate': 1},
-#     ]
-#
-#     def tick(self) -> Tuple[int, float] | Tuple[int, float, List[type[User]] | None] | None:
-#         run_time = self.get_run_time()
-#
-#         for stage in self.stages:
-#             if run_time < stage['duration']:
-#                 tick_data = (stage['users'], stage['spawn_rate'])
-#                 return tick_data
-#             return None
 
 
 @events.test_start.add_listener

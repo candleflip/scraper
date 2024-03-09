0b91b983423cda91126b2fa032e8f799de16ef32
Sergei Svechnikov <srgcandle@mail.ru>
2024-03-09 11:07:44 +0400

    style(product): Перевести логи в исполняемом файле.

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "Waiting for postgres..."
+echo "Ожидание Postgres..."
 
 while ! nc -z web-db 5432; do
   sleep 0.1
 done
 
-echo "PostgreSQL started"
+echo "Postgres стартовал!"
 
 exec "$@"

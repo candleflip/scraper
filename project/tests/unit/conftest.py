cc701ee54411aee29a80d8e94d5046004c7df8ea
Sergei Svechnikov <srgcandle@mail.ru>
2024-03-09 14:20:50 +0400

    style(tests): Добавить докстроки в модуль фикстур.

--- project/tests/unit/conftest.py
+++ project/tests/unit/conftest.py
@@ -1,3 +1,9 @@
+"""
+Модуль с набором фикстур для тестирования.
+
+Использовать в файлах с unit-тестами.
+
+"""
 import os
 
 import pytest
@@ -9,11 +15,28 @@ from app.main import create_application
 
 
 def get_settings_override():
+    """
+    Заменить настройки для тестовой среды
+
+    Returns:
+        Тестовые настройки.
+
+    """
     return Settings(testing=1, database_url=os.environ.get('DATABASE_TEST_URL'))
 
 
 @pytest.fixture(scope='module')
 def test_app():
+    """
+    Подготовить экземпляр тестового приложения
+
+    Создать приложение, передать тестовые настройки и создать клиент
+    для тестирования его работы.
+
+    Returns:
+        Клиент для тестирования приложения
+
+    """
     app = create_application()
     app.dependency_overrides[get_settings] = get_settings_override
     with TestClient(app) as test_client:
@@ -22,6 +45,16 @@ def test_app():
 
 @pytest.fixture(scope='module')
 def test_app_with_db():
+    """
+    Подготовить экземпляр тестового приложения с БД
+
+    Создать приложение, передать тестовые настройки и подготовить тестовую БД.
+    А также создать клиент для тестирования работы приложения.
+
+    Returns:
+        Клиент для тестирования приложения с БД
+
+    """
     app = create_application()
     app.dependency_overrides[get_settings] = get_settings_override
     register_tortoise(

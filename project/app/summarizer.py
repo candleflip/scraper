9a03adfd8b07b87fd3c5137e5de8413f761a7f44
Sergei Svechnikov <srgcandle@mail.ru>
2024-03-10 15:38:21 +0400

    style(apps): Добавить документацию к модулю работы с пересказами.

--- project/app/summarizer.py
+++ project/app/summarizer.py
@@ -1,3 +1,6 @@
+"""
+Модуль работы с пересказами
+"""
 import nltk
 from newspaper import Article
 
@@ -5,6 +8,17 @@ from app.models.tortoise.summary_schema import TextSummary
 
 
 async def generate_summary(summary_id: int, url: str) -> None:
+    """
+    Генерация пересказа текста на WEB-странице по ее URL
+
+    Загрузка и парсинг страницы по переданному URL. Подготовка краткого пересказа
+    текста, найденного на странице
+
+    Args:
+        summary_id: ID пересказа, заготовка для которого была добавлена в БД ранее
+        url: путь до WEB-страницы, на которой расположен текст
+
+    """
     article = Article(url=url)
     article.download()
     article.parse()

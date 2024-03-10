6f2389c603b365348b3d2aa16cfa47f14df639b2
Sergei Svechnikov <srgcandle@mail.ru>
2024-03-10 15:29:56 +0400

    style(apps): Добавить документацию к вспомогательному модулю с функционалом конфигурации БД.

--- project/app/db.py
+++ project/app/db.py
@@ -1,3 +1,6 @@
+"""
+Вспомогательный модуль для подготовки БД к работе приложения
+"""
 import logging
 import os
 
@@ -7,6 +10,7 @@ from tortoise.contrib.fastapi import register_tortoise
 
 log = logging.getLogger('uvicorn')
 
+# Конфигурация TortoiseORM для приложения
 TORTOISE_ORM = {
     'connections': {'default': os.environ.get('DATABASE_URL')},
     'apps': {
@@ -19,6 +23,13 @@ TORTOISE_ORM = {
 
 
 def initialize_database(app: FastAPI) -> None:
+    """
+    Инициализирует БД для приложения с моделями из файла Tortoise моделей
+
+    Args:
+        app: приложение, использующее БД
+
+    """
     register_tortoise(
         app=app,
         db_url=os.environ.get('DATABASE_URL'),
@@ -29,10 +40,17 @@ def initialize_database(app: FastAPI) -> None:
 
 
 async def generate_schema() -> None:
-    log.info('Tortoise initialization...')
+    """
+    Создание схемы по пути до файла с Tortoise моделями
 
-    await Tortoise.init(db_url=os.environ.get('DATABASE_URL'), modules={'models': ['models.tortoise.summary_schema']})
-    log.info('Generating database schema via Tortoise...')
+    """
+    log.info('Инициализация Tortoise...')
+    await Tortoise.init(
+        db_url=os.environ.get('DATABASE_URL'),
+        modules={'models': ['models.tortoise.summary_schema']}
+    )
+
+    log.info('Генерация схемы БД через Tortoise...')
     await Tortoise.generate_schemas()
     await Tortoise.close_connections()
 

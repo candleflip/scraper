175cc8325505aa447263aa7c8d66bf3993bda6d5
Sergei Svechnikov <srgcandle@mail.ru>
2024-03-10 15:23:03 +0400

    style(tests): Добавить документацию к модулю нагрузочного тестирования.

--- project/tests/load/dev.py
+++ project/tests/load/dev.py
@@ -1,9 +1,26 @@
+"""
+Модуль конфигурации нагрузочного тестирования в Locust
+
+URL с интерфейсом: http://localhost:8005
+"""
 from __future__ import annotations
+import logging
 
 from locust import HttpUser, between, events, task
 
+log = logging.getLogger('locust')
+
 
 class WebUser(HttpUser):
+    """
+    Класс конфигурации пользователей, генерируемых для нагрузочного теста.
+
+    wait_time: ожидание между соседними запросами от экземпляров пользователей,
+        созданных из этого класса (в секундах)
+    weight: вес данного типа пользователей в системе
+    host: хост, по которому находится тестируемое приложение
+
+    """
     wait_time = between(1, 5)
     weight = 3
     host = 'http://0.0.0.0:8005'
@@ -15,9 +32,17 @@ class WebUser(HttpUser):
 
 @events.test_start.add_listener
 def ot_test_start(environment):
-    print('A new test is starting...')
+    """
+    Логирует начало теста.
+
+    """
+    log.info('Старт нового нагрузочного теста...')
 
 
 @events.test_stop.add_listener
 def ot_test_stop(environment):
-    print('A new test is stopping...')
+    """
+    Логирует окончание теста.
+
+    """
+    log.info('Остановка нагрузочного теста...')
